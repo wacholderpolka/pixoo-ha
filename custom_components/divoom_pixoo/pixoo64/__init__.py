7d6ab73d169f1576e85b7c2cc331334a19c5abfd
wacholderpolka <117754277+wacholderpolka@users.noreply.github.com>
2024-02-11 16:05:36 +0100

    Update __init__.py
    New font integration

--- custom_components/divoom_pixoo/pixoo64/__init__.py
+++ custom_components/divoom_pixoo/pixoo64/__init__.py
@@ -1,3 +1,3 @@
-from ._pixoo import Pixoo, FONT_GICKO, FONT_PICO_8
+from ._pixoo import Pixoo, FONT_GICKO, FONT_PICO_8, FONT_THIN
 from ._colors import Palette
-from ._font import retrieve_glyph, FONT_GICKO, FONT_PICO_8
+from ._font import retrieve_glyph, FONT_GICKO, FONT_PICO_8, FONT_THIN
