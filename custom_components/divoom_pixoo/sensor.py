5833541b049b375af724970a3f2e3a3f17218932
wacholderpolka <117754277+wacholderpolka@users.noreply.github.com>
2024-02-15 08:10:16 +0100

    Update sensor.py

--- custom_components/divoom_pixoo/sensor.py
+++ custom_components/divoom_pixoo/sensor.py
@@ -161,7 +161,7 @@ class Pixoo64(Entity):
                     if text['font'] == "FONT_GICKO":
                         pixoo.draw_text(rendered_text.upper(), tuple(text['position']), tuple(text['font_color']), FONT_GICKO)
                     if text['font'] == "FONT_THIN":
-                        pixoo.draw_text(rendered_text.upper(), tuple(text['position']), tuple(text['font_color']), FONT_THIN)
+                        pixoo.draw_text(rendered_text, tuple(text['position']), tuple(text['font_color']), FONT_THIN)
                 pixoo.push()
 
             if "PV" in current_page_data:
